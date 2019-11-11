11dd6640188db13f80da59449878148e142891e8
unknown <davidvazquez1026@gmail.com>
2019-11-10 23:27:08 -0600

    (first working version)

--- versionSorter/src/app/app.component.spec.ts
+++ versionSorter/src/app/app.component.spec.ts
@@ -26,36 +26,52 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('versionSorter');
   });
 
-describe('isNumber()', () => {
-  it('it should return true for a vaild number', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.isNumber('4')).toBeTruthy();
+  describe('isNumber()', () => {
+    it('it should return true for a vaild number', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.isNumber('4')).toBeTruthy();
+    })
+    it('it should return true for a vaild number'), () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.isNumber('a')).toBeFalsy();
+    }
   })
-  it('it should return true for a vaild number'), () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.isNumber('a')).toBeFalsy();
-  }
-})
 
-describe('parseVersion', () => {
-  it('should return null if too many parts', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.parseVersion('4.4.4.4')).toBeNull();
+  describe('parseVersion()', () => {
+    it ('should return null if too many parts', () => {
+      const app = TestBed.createComponent(AppComponent).componentInstance;
+      expect(app.parseVersion("4.4.4.4")).toBeNull();
+    })
+    it ('should return null if NaN', () => {
+      const app = TestBed.createComponent(AppComponent).componentInstance;
+      expect(app.parseVersion("a.4.4")).toBeNull();
+    })
+    it ('should return null if empty string', () => {
+      const app = TestBed.createComponent(AppComponent).componentInstance;
+      expect(app.parseVersion('')).toBeNull(); 
+    })
   })
-  it('should return null of NaN', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.parseVersion('a.4.4')).toBeNull();
+
+  describe ('compareVersion()', () => {
+    it ('should return true if > and false if <', () => {
+      const app = TestBed.createComponent(AppComponent).componentInstance;
+      expect(app.compareVersion([2,1,1], [1,1,1])).toBeTruthy();
+      expect(app.compareVersion([2,1,1], [1,2,1])).toBeTruthy();
+      expect(app.compareVersion([2,2,1], [2,1,1])).toBeTruthy();
+      expect(app.compareVersion([2,1,1], [2,1,1])).toBeTruthy();
+      expect(app.compareVersion([1,2,2], [1,2,1])).toBeTruthy();
+      expect(app.compareVersion([1,2,1], [1,2,2])).toBeFalsy();
+      expect(app.compareVersion([2], [1,1,1])).toBeTruthy();
+      expect(app.compareVersion([2], [2])).toBeTruthy();
+      expect(app.compareVersion([2,1], [2])).toBeTruthy();
+      expect(app.compareVersion([2], [2,1])).toBeFalsy();
+    })
   })
-  it('should return null if empty', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.parseVersion('')).toBeNull();
-  })
-})
+
+
+
 
 
 });
