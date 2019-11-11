4fec0c950fe1719ea5da09adefa46f8a92502ffb
unknown <davidvazquez1026@gmail.com>
2019-11-10 23:27:08 -0600

    (first working versoin!)

--- versionSorter/src/app/app.component.ts
+++ versionSorter/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'versionSorter';
 
@@ -56,7 +57,39 @@ export class AppComponent {
   ]
 
   isNumber(x: string): boolean  {
-    return x !== "" && !isNaN(Number(x))
+  return x !== "" && !isNaN(Number(x))
+  }
+
+  parseVersion(version: string): number[] | null {
+    const parts = version.split('.')
+    if (parts.length > 3) return null;
+    if (!parts.every(this.isNumber)) return null 
+    const numParts = parts.map(x => Number(x))         
+    return numParts;
+  }
+
+  compareVersion(version1: number[], version2: number[]): boolean {
+    const [a1, a2, a3] = version1;
+    const [b1, b2, b3] = version2;
+    if (a1 > b1) return true;
+    if (a1 < b1) return false;
+    if (a2 > b2) return true;
+    if (a2 < b2) return false;
+    if (a3 > b3) return true;
+    if (a3 < b3) return false;
+    return true;
+  }
+
+  filterData(inputVersion: string): { name: string; version: string}[] {
+    const parsedVersion = this.parseVersion(inputVersion)
+    if (parsedVersion === null)
+    return null;
+    const filteredVersion = this.data.filter(obj => this.compareVersion(this.parseVersion(obj.version), parsedVersion))
+    return filteredVersion;
+  }
+
+  onChangeVersion(event) {
+    console.log(this.filterData(event.target.value))
   }
 
 }
